082c5a0b59d22d48a964304a81b4a0954cb54dce
Shashank <s_shashank@outlook.com>
2019-07-12 18:17:56 +0530

    styling the app

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,27 +1,54 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 /* console.log(__dirname);
 console.log(path.join(__dirname, "../public")); */
 
 const app = express();
 
+// set up hbs template engine for express
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "../templates/views"));
+hbs.registerPartials(path.join(__dirname, "../templates/partials"));
+
+// setup static directries to server
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("", (req, res) => {
-  res.send("<h1>Hello Express!</h1>");
+  res.render("index", {
+    title: "Weather App"
+  });
 });
 
 app.get("/help", (req, res) => {
-  res.send("Help page!");
+  res.render("help", {
+    title: "Weather App - Help"
+  });
 });
 
 app.get("/about", (req, res) => {
-  res.send("About page!");
+  res.render("about", {
+    title: "Weather App - About"
+  });
 });
 
 app.get("/weather", (req, res) => {
   res.send("Weather page!");
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404 Page",
+    error: "Help article not found"
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404 Page",
+    error: "Page not found"
+  });
 });
 
 app.listen(3000, () => {
